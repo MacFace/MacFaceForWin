a485b2ad22215f62960c53cc0055e42cb4e0edb5
rryu <rryu@t3.rim.or.jp>
2009-02-20 14:51:59 +0000

    MacFace::MachineStatusクラスをとりあえず追加。

---
+++ MacFaceLibrary/MachineStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MacFace
+{
+    class MachineStatus
+    {
+        protected static MachineStatus sharedMachineStatus;
+
+        protected Byte numberProcessors;
+
+        public static MachineStatus LocalMachineStatus()
+        {
+            if (sharedMachineStatus == null)
+            {
+                sharedMachineStatus = new MachineStatus();
+            }
+            return sharedMachineStatus;
+        }
+
+        protected MachineStatus()
+        {
+            NtKernel.SYSTEM_BASIC_INFORMATION info = NtKernel.QuerySystemBasicInformation();
+            numberProcessors = info.NumberProcessors;
+        }
+
+        public int NumberProcessors
+        {
+            get
+            {
+                return numberProcessors;
+            }
+        }
+
+    }
+}
